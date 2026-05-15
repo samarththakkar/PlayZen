94bbffe4d3e61ef4ec3feb74f148630de4905eb5
Samarth <samarththakkar841@gmail.com>
2026-05-15 11:00:57 +0530

    update healthcheck route path from root to /healthcheck

--- backend/src/routes/healthcheck.routes.js
+++ backend/src/routes/healthcheck.routes.js
@@ -3,6 +3,6 @@ import { healthcheck } from "../controllers/healthcheck.controller.js";
 
 const router = Router();
 
-router.route("/").get(healthcheck);
+router.route("/healthcheck").get(healthcheck);
 
 export default router;
